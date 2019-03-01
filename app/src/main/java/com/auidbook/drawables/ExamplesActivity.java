1cf79b8bff9dd557428069d42a1a15dd5441c07f
Artem Vaganov <me@arvalon.ru>
2019-03-01 16:36:40 +0300

    Add new main activity

---
+++ app/src/main/java/com/auidbook/drawables/ExamplesActivity.java
@@ -0,0 +1,65 @@
+package com.auidbook.drawables;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.ColorFilter;
+import android.graphics.LightingColorFilter;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+/**
+ * Drawables example
+ *
+ * SimpleTextDrawable and BetterTextDrawable replaced by BetterTextView
+ *
+ * @author Ian G. Clifton
+ * @author arvalon
+ */
+public class ExamplesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_examples);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // SimpleImageDrawable
+
+        View view = findViewById(R.id.view);
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.giwajip);
+        SimpleImageDrawable simpleImageDrawable = new SimpleImageDrawable(bitmap);
+        view.setBackground(simpleImageDrawable);
+
+        // LightingColorFilter
+        int mul = 0xFF00FF00;
+        int add = 0x000000BB;
+
+        ColorFilter lightingColorFilter = new LightingColorFilter(mul, add);
+        View view2 = findViewById(R.id.view2);
+        SimpleImageDrawable simpleImageDrawable2 = new SimpleImageDrawable(bitmap);
+        view2.setBackground(simpleImageDrawable2);
+        simpleImageDrawable2.setColorFilter(lightingColorFilter);
+
+        // RoundedImageDrawable
+
+        Bitmap largeBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.giwajip);
+
+        ImageView imageView = findViewById(R.id.image);
+        RoundedImageDrawable roundedImageDrawable = new RoundedImageDrawable(largeBitmap, 200);
+        imageView.setImageDrawable(roundedImageDrawable);
+
+        // FadedImageDrawable
+
+        ImageView imageView2 = findViewById(R.id.image2);
+        FadedImageDrawable fadedImageDrawable = new FadedImageDrawable(largeBitmap);
+        imageView2.setImageDrawable(fadedImageDrawable);
+
+    }
+}
--- app/src/main/java/com/auidbook/drawables/MainActivity.java
+++ app/src/main/java/com/auidbook/drawables/MainActivity.java
@@ -3,63 +3,27 @@ package com.auidbook.drawables;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.ColorFilter;
-import android.graphics.LightingColorFilter;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageView;
 
-/**
- * Drawables example
- *
- * SimpleTextDrawable and BetterTextDrawable replaced by BetterTextView
- *
- * @author Ian G. Clifton
- * @author arvalon
- */
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        // SimpleImageDrawable
+        findViewById(R.id.examples_button).setOnClickListener(v ->
+                startActivity(new Intent(this,ExamplesActivity.class))
+        );
 
-        View view = findViewById(R.id.view);
-        Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.giwajip);
-        SimpleImageDrawable simpleImageDrawable = new SimpleImageDrawable(bitmap);
-        view.setBackground(simpleImageDrawable);
-
-        // LightingColorFilter
-        int mul = 0xFF00FF00;
-        int add = 0x000000BB;
-
-        ColorFilter lightingColorFilter = new LightingColorFilter(mul, add);
-        View view2 = findViewById(R.id.view2);
-        SimpleImageDrawable simpleImageDrawable2 = new SimpleImageDrawable(bitmap);
-        view2.setBackground(simpleImageDrawable2);
-        simpleImageDrawable2.setColorFilter(lightingColorFilter);
-
-        // RoundedImageDrawable
-
-        Bitmap largeBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.giwajip);
-
-        ImageView imageView = findViewById(R.id.image);
-        RoundedImageDrawable roundedImageDrawable = new RoundedImageDrawable(largeBitmap, 200);
-        imageView.setImageDrawable(roundedImageDrawable);
-
-        // FadedImageDrawable
-
-        ImageView imageView2 = findViewById(R.id.image2);
-        FadedImageDrawable fadedImageDrawable = new FadedImageDrawable(largeBitmap);
-        imageView2.setImageDrawable(fadedImageDrawable);
-
+        findViewById(R.id.try_button).setOnClickListener(v ->{
+            // TODO: 01.03.2019 Simple Drawable
+                }
+        );
     }
 }
